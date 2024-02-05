8dd5a0a40905ab2ff92439c8692522bb15361c3e
yogevgit10 <yogev.git@runi.ac.il>
2024-02-06 00:57:51 +0200

    yogev

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -6,48 +6,43 @@ public class HashTagTokenizer {
 
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
-		//breakHashTag(hashTag, dictionary);
-		breakHashTag(hashTag,dictionary);
 
+		breakHashTag(hashTag, dictionary);
 		
+		//for (int i = 0; i < dictionary.length; i++)
+		//{
+		//	System.out.println(dictionary[i]);
+		//}
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
+		
 		In in = new In(fileName);
-
-		for (int i = 0;i < 3000 ;i++ ) 
+		
+		for (int i = 0; i < dictionary.length; i++)
 		{
-			dictionary[i] = in.readLine();	
+			dictionary[i] = in.readLine();
 		}
+		
+
+		// Your code here
 
 		return dictionary;
 	}
 
-	public static void print (String [] array)
-	{
-		for (int i = 0;i < array.length ;i++ ) 
-		{
-			System.out.println(array[i]);	
-		}
-	}
-
-
-
-
 	public static boolean existInDictionary(String word, String []dictionary) {
-		for (int i = 0;i < dictionary.length ;i++ ) 
-		{
-			if (dictionary[i].equals(word)) 
-			{
-				return true;			
-			}	
-		}
-		return false;
+		
+          for (int i = 0; i < dictionary.length; i++)
+		  {
+			if (word.equals(dictionary[i])) return true;
+		  }
+		  return false;
+        
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+		String newword = "";
 
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
@@ -56,17 +51,17 @@ public class HashTagTokenizer {
  
         int N = hashtag.length();
 
-        for (int i = 1; i <= N; i++) 
-        {
-			String temp = hashtag.substring(0,i);
-            if (existInDictionary(temp, dictionary)) {
-          
-                System.out.println(temp);
-                breakHashTag(hashtag.substring(i,N), dictionary);
-                
+        for (int i = 1; i <= N; i++) {
+            newword = newword + hashtag.charAt(i-1);
+			if (existInDictionary(newword, dictionary)==true) 
+			{
+				System.out.print(newword + " ");
+				
+				breakHashTag(hashtag.substring(i), dictionary);
+		
             }
-			
-        }
-      }
+		}
+		
+    }
 
 }
